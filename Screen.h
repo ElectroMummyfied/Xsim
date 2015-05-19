87acaff1d940cfcba9d06d391cdea77a91430b65
ElectroMummyfied <electromummyfied@gmail.com>
2015-05-18 22:32:16 -0400

    Create Screen.h

---
+++ Screen.h
@@ -0,0 +1,18 @@
+#include <SDL.h>
+#ifndef SCREEN_H_INCLUDED
+#define SCREEN_H_INCLUDED
+
+class Screen {
+    private:
+        SDL_Window *window;
+        SDL_Renderer *renderer;
+        int width;
+        int height;
+        int centerX;
+        int centerY;
+    public:
+        Screen(int,int);
+        ~Screen();+
+};
+
+#endif // SCREEN_H_INCLUDED
